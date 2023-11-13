394254523fc8cd795d9275539a28345900791c21
Oleksandr Buhai <oleksandr.buugay@gmail.com>
2023-11-12 22:40:44 -0700

    1st(Initialization packge, add assets)

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Test } from "./components/Com"
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='text-red-500 justify-center flex'>
+      Hello world
+      <Test/>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
